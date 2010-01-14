7e9d23a451f3a279fae1083c0d8ffc992096c7b5
Diego Carrion <dc.rec1@gmail.com>
2010-01-14 20:53:19 -0200

    configured new relic

--- start.sh
+++ start.sh
@@ -1,1 +1,2 @@
+export JAVA_OPTIONS="${JAVA_OPTIONS} -javaagent:$(pwd)/newrelic/newrelic.jar"
 java -server -d64 -Xmx1024M -Xms64M -Djetty.logs="./logs" -Dsolr.solr.home="./solr" -Dsolr.data.dir="./data" -Djetty.port=8983 -jar start.jar &
