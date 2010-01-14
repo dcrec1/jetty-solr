2f81911dd3e0eb6f2088a3c96393223240cbd79d
Diego Carrion <dc.rec1@gmail.com>
2010-01-14 21:13:55 -0200

    fixed how newrelic is loaded

--- start.sh
+++ start.sh
@@ -1,2 +1,1 @@
-export JAVA_OPTIONS="${JAVA_OPTIONS} -javaagent:$(pwd)/newrelic/newrelic.jar"
-java -server -d64 -Xmx1024M -Xms64M -Djetty.logs="./logs" -Dsolr.solr.home="./solr" -Dsolr.data.dir="./data" -Djetty.port=8983 -jar start.jar &
+java -server -d64 -Xmx1024M -Xms64M -javaagent:$(pwd)/newrelic/newrelic.jar -Djetty.logs="./logs" -Dsolr.solr.home="./solr" -Dsolr.data.dir="./data" -Djetty.port=8983 -jar start.jar &
