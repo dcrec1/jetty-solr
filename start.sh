9046890821aa2479bc7c6f734c3243771b671dd4
Diego Carrion <dc.rec1@gmail.com>
2010-01-31 16:04:39 -0200

    removed start.sh in favour of start. Run like this: ./start ./start test ./start production

--- start.sh
+++
@@ -1,1 +0,0 @@
-java -server -d64 -Xmx1024M -Xms64M -javaagent:$(pwd)/newrelic/newrelic.jar -Djetty.logs="./logs" -Dsolr.solr.home="./solr" -Dsolr.data.dir="./data" -Djetty.port=8983 -jar start.jar &
